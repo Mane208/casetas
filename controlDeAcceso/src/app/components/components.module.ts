9a19c3b21d99e9ba0379385031729d277e07416b
agrishurtado <agrishurtado@gmail.com>
2020-07-27 18:32:49 -0500

    cambios en color a textos intercambio de botos en orden requerido por cliente en submenus

--- controlDeAcceso/src/app/components/components.module.ts
+++ controlDeAcceso/src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
-import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
 
 
@@ -10,11 +9,9 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     HeaderComponent,
-    MenuComponent
   ],
   exports:[
     HeaderComponent,
-    MenuComponent
   ],
   imports: [
     CommonModule,
--- controlDeAcceso/src/app/pages/btn-inicio/btn-inicio.page.ts
+++ controlDeAcceso/src/app/pages/btn-inicio/btn-inicio.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-btn-inicio',
@@ -8,12 +8,9 @@ import { MenuController } from '@ionic/angular';
 })
 export class BtnInicioPage implements OnInit {
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor() { }
 
   ngOnInit() {
   }
-  toggleMenu(){
-    this.menuCtrl.toggle();
-
-  }
+  
 }
